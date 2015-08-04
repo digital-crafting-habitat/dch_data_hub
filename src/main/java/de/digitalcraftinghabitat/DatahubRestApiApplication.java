99ea25433310f3aa1021289a21f9ac02fb7dcc64
Christopher Ezell <raversound20@web.de>
2015-08-04 22:06:27 +0200

    Initial Spring Apllication added (gradle build )

---
+++ src/main/java/de/digitalcraftinghabitat/DatahubRestApiApplication.java
@@ -0,0 +1,12 @@
+package de.digitalcraftinghabitat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatahubRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatahubRestApiApplication.class, args);
+    }
+}
---
+++ src/test/java/de/digitalcraftinghabitat/DatahubRestApiApplicationTests.java
@@ -0,0 +1,18 @@
+package de.digitalcraftinghabitat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DatahubRestApiApplication.class)
+@WebAppConfiguration
+public class DatahubRestApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
