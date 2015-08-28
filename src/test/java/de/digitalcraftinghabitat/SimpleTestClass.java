174cb354f5ca672619c59e3410d16f1231f8ac3a
Christopher Ezell <raversound20@web.de>
2015-08-28 18:39:08 +0200

    removed SpringBoot Tests

--- src/test/java/de/digitalcraftinghabitat/SimpleTestClass.java
+++ src/test/java/de/digitalcraftinghabitat/SimpleTestClass.java
@@ -2,19 +2,18 @@ package de.digitalcraftinghabitat;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import static junit.framework.Assert.assertTrue;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = DatahubRestApiApplication.class)
-@WebAppConfiguration
-public class DatahubRestApiApplicationTests {
+@RunWith(JUnit4.class)
+public class SimpleTestClass {
 
 	@Test
-	public void contextLoads() {
+	public void simpleTest() {
 		assertTrue(true);
 	}
 
