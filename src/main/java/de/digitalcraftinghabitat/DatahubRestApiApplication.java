d73b2d055e363d603d21287c0e096ab8ca70bd66
Christopher Ezell <raversound20@web.de>
2015-08-28 17:01:24 +0200

    added Debug Code

--- src/main/java/de/digitalcraftinghabitat/DatahubRestApiApplication.java
+++ src/main/java/de/digitalcraftinghabitat/DatahubRestApiApplication.java
@@ -8,5 +8,12 @@ public class DatahubRestApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DatahubRestApiApplication.class, args);
+
+        /*System.out.println("Let's inspect the beans provided by Spring Boot:");
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }*/
     }
 }
